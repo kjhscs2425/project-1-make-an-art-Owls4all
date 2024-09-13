11b9b7f620214e989a98ae278f42e263f51c18d1
owls4all <owls4all@gmail.com>
2024-09-13 09:28:18 -0700

    started setting up functions

--- main.py
+++ main.py
@@ -7,17 +7,30 @@ alpha = 30  #
 
 s = 100 #side length variable 
 triCount = 3 #number of triangles               these can change!
-triScale = 0.5 #Relative scale of triangle sides
+triScale = 0.75 #Relative scale of triangle sides
+triSpread = 0.5 #affects distance of triangles
 #----------------------------------------#
 
 #---------setting up functions-----------#
 def hexagon(side):
     t.forward(side)
-    t.left(theta)
+    t.right(theta)
     for i in range(5):
         t.forward(side)
-        
+        t.right(phi)
+    t.left(theta)
+    t.forward(side)
 
+def travel():
+    t.up()
+    t.left(180-theta+alpha)
+    t.forward(s*(triSpread/triCount))
+    t.left(alpha)
+    t.down()
 
+def triangle(whichOne):
+    for i in range(3):
+        t.forward(s*triScale**whichOne)
+        t.left(theta)
 
 turtle.exitonclick()
