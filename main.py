50693f1b2d203a97e5fa885a048a716a8975b3e4
owls4all <owls4all@gmail.com>
2024-09-13 14:19:34 -0700

    added comments

--- main.py
+++ main.py
@@ -1,49 +1,56 @@
 import turtle
 # ---------Setting up variables ---------#
-t = turtle #typing shortcut
-theta = 120 # 
-phi = 60    #   angles- these shouldn't be changed
-alpha = 30  # 
+#stuff to mess with 
+sides = 6 #number of sides. (default 6) Changing this not yet fully supported
+s = 100 #side length variable (default 100)
+triCount = 12 #number of triangles  (default 3)       
+triScale = 0.85 #Relative scale of triangle sides (default 0.85)
+triSpread = 0.75 #affects distance of triangles (default 0.75)
 
-s = 100 #side length variable 
-triCount = 3 #number of triangles               these can change!
-triScale = 0.75 #Relative scale of triangle sides
-triSpread = 0.5 #affects distance of triangles
+#important stuff- best not to change
+t = turtle  # typing shortcut
+theta = 720/sides # angle to turn for triangle edges
+phi = theta/2    # angle to turn for hexagon edges
+alpha = phi/2  # angle from edge to center of gap
 #----------------------------------------#
 
 #---------setting up functions-----------#
-def hexagon(side):
-    t.forward(side)
-    t.left(theta)
-    for i in range(5):
-        t.forward(side)
-        t.left(phi)
-    t.right(theta+180)
-    t.forward(side)
+def shape(side,sideCount): # function count 1
+    t.forward(side) #draw line to edge of hexagon
+    t.left(theta) #turn to draw the first side
+    for i in range(1,sideCount): # loop count 1
+        t.forward(side) #draw a side 
+        t.left(phi) #turn to draw next side
+    t.right(theta+180) #turn to center
+    t.forward(side) #finish shape
 
-def travel():
-    t.up()
-    t.left(phi-alpha)
-    t.forward(s*(triSpread/triCount))
-    t.right(alpha)
-    t.down()
+def travel(): # function count 2
+    t.up() #stop drawing
+    t.left(phi-alpha) #point to center of gap
+    t.forward(s*(triSpread/triCount)) #create distance from center
+    t.right(alpha) #point in correct direction for triangle drawing
+    t.down() #start drawing again
     
 
-def triangle(whichOne):
-    for i in range(3):
-        t.forward(s*triScale**whichOne)
-        t.left(theta)
+def triangle(whichOne): # function count 3
+    for i in range(3): # loop count 2
+        t.forward(s*triScale**whichOne) #draw triangle side, scaled by appropriate factor
+        t.left(theta) #turn to draw next side
 #-------------------------------------#
 
 #-----------begin executing-----------#
-t.left(phi)
-hexagon(s)
-t.left(180)
-travel()
+t.left(phi) #prepare initial angle
+shape(s,sides) #draw the hexagon
+t.left(180) #point opposite direction
+travel() #move to start point for first triangle
 
-for i in range(1,triCount):
-    travel()
-    triangle(i)
+for i in range(1,triCount): # loop count 3
+    triangle(i) #draw a triangle. Use loop variable to specify which size
+    travel() #prepare for next triangle
+
+for i in range(3): # loop count 3
+        t.forward(s*triScale**triCount+1) #draw side of final triangle
+        t.left(theta) #turn to draw next side
 #-------------------------------------#
 
 #--------Make the image persist-------#
