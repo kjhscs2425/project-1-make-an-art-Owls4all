f106d283e425369e88efaef21f40c21420154724
owls4all <owls4all@gmail.com>
2024-09-13 09:16:05 -0700

    began setting up variables and functions

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 import turtle
+# ---------Setting up variables ---------#
+t = turtle #typing shortcut
+theta = 120 # 
+phi = 60    #   angles- these shouldn't be changed
+alpha = 30  # 
 
-turtle.forward(100)
+s = 100 #side length variable 
+triCount = 3 #number of triangles               these can change!
+triScale = 0.5 #Relative scale of triangle sides
+#----------------------------------------#
+
+#---------setting up functions-----------#
+def hexagon(side):
+    t.forward(side)
+    t.left(theta)
+    for i in range(5):
+        t.forward(side)
+        
+
+
+
 turtle.exitonclick()
